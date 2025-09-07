58155da5c30dc860d0d553552295dad0f380e29e
Minze <gustavogarone@protonmail.com>
2025-09-07 18:48:23 -0300

    Primeira versão funcional

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import cv2
 import numpy as np
 
@@ -5,102 +7,182 @@ import utlis
 
 ALTURA = 700
 LARGURA = 700
-
-path = "./retacheia.jpg"
-img = cv2.imread(path)
-
-# Processando imagem
-img = cv2.resize(img, (LARGURA, ALTURA))
-imgContorno = img.copy()
-imgInteresses = img.copy()
-imgCinza = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-imgBlur = cv2.GaussianBlur(imgCinza, (5, 5), 10)
-imgCanny = cv2.Canny(imgBlur, 10, 50)
-
-# Desenhando contornos
-contornos, hierarchy = cv2.findContours(
-    imgCanny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
-)
-cv2.drawContours(imgContorno, contornos, -1, (0, 255, 0), 2)
-
-# Encontrando retângulos de interesse
-retangulos = utlis.retContorno(contornos)
-pontasRetanguloGabarito = utlis.reordenar(utlis.encontraPontas(retangulos[0]))
-pontasRetanguloId = utlis.reordenar(utlis.encontraPontas(retangulos[1]))
-
-# Arrumando perspectiva
-pt1Gabarito = np.float32(pontasRetanguloGabarito)
-pt2Gabarito = np.float32([[0, 0], [LARGURA, 0], [0, ALTURA], [LARGURA, ALTURA]])
-transformacaoGabarito = cv2.getPerspectiveTransform(pt1Gabarito, pt2Gabarito)
-imgGabaritoArrumada = cv2.warpPerspective(img, transformacaoGabarito, (LARGURA, ALTURA))
-
-pt1Id = np.float32(pontasRetanguloId)
-pt2Id = np.float32([[0, 0], [LARGURA, 0], [0, ALTURA], [LARGURA, ALTURA]])
-transformacaoId = cv2.getPerspectiveTransform(pt1Id, pt2Id)
-imgIdArrumada = cv2.warpPerspective(img, transformacaoId, (LARGURA, ALTURA))
-
-# Exibir retângulos detectados (TEST)
-if pontasRetanguloGabarito.size != 0 and pontasRetanguloId.size != 0:
-    cv2.drawContours(imgInteresses, pontasRetanguloGabarito, -1, (255, 0, 0), 5)
-    cv2.drawContours(imgInteresses, pontasRetanguloId, -1, (0, 0, 255), 5)
+LIMITE_GABARITO = 1900
+LIMITE_ID = 3100
 
 
-# Limiar marcados
-imgIdCinza = cv2.cvtColor(imgIdArrumada, cv2.COLOR_BGR2GRAY)
-imgIdLimiar = cv2.threshold(imgIdCinza, 170, 255, cv2.THRESH_BINARY_INV)[1]
-imgGabaritoCinza = cv2.cvtColor(imgGabaritoArrumada, cv2.COLOR_BGR2GRAY)
-imgGabaritoLimiar = cv2.threshold(imgGabaritoCinza, 170, 255, cv2.THRESH_BINARY_INV)[1]
+def main():
+    utlis.preparaCSV()
+    with os.scandir("./Provas/") as es:
+        for e in es:
+            print(f"Analisando prova {e.path}")
+            analisaProva(str(e.path))
+    return None
 
-# Ler Gabarito
-marcs = utlis.separarGabarito(imgGabaritoLimiar)
-# Isso vai precisar de melhorias! Dropa as colunas "inúteis" (espaços em branco,
-# números de questões)
-i = 0
-while i <= (utlis.COLUNAS_GABARITO - 1) * utlis.ALTERNATIVAS:
-    marcs = np.delete(marcs, i, 1)  # Deleta a coluna inútil
-    i += utlis.ALTERNATIVAS
 
-respostas = np.zeros(utlis.QUESTOES)
+def processaProvas(img):
+    img = cv2.resize(img, (LARGURA, ALTURA))
+    imgContorno = img.copy()
+    imgInteresses = img.copy()
+    imgCinza = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgCinza, (5, 5), 10)
+    imgCanny = cv2.Canny(imgBlur, 10, 50)
 
-k = 0
-while k < utlis.COLUNAS_GABARITO:  # Itera colunas
-    j = 0
-    while j < utlis.QUESTOES_POR_COLUNA:  # Itera linhas na coluna
+    # Desenhando contornos
+    contornos, hierarchy = cv2.findContours(
+        imgCanny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
+    )
+    cv2.drawContours(imgContorno, contornos, -1, (0, 255, 0), 2)
+
+    # cv2.imshow("Contornos", imgContorno)
+    # while True:
+    #     if cv2.waitKey(25) & 0xFF == ord("q"):
+    #         break
+
+    # Encontrando retângulos de interesse
+    retangulos = utlis.retContorno(contornos)
+    pontasRetanguloGabarito = utlis.reordenar(utlis.encontraPontas(retangulos[0]))
+    pontasRetanguloId = utlis.reordenar(utlis.encontraPontas(retangulos[1]))
+
+    # Arrumando perspectiva
+    pt1Gabarito = np.float32(pontasRetanguloGabarito)
+    pt2Gabarito = np.float32([[0, 0], [LARGURA, 0], [0, ALTURA], [LARGURA, ALTURA]])
+    transformacaoGabarito = cv2.getPerspectiveTransform(pt1Gabarito, pt2Gabarito)
+    imgGabaritoArrumada = cv2.warpPerspective(
+        img, transformacaoGabarito, (LARGURA, ALTURA)
+    )
+
+    pt1Id = np.float32(pontasRetanguloId)
+    pt2Id = np.float32([[0, 0], [770, 0], [0, 770], [770, 770]])
+    transformacaoId = cv2.getPerspectiveTransform(pt1Id, pt2Id)
+    imgIdArrumada = cv2.warpPerspective(img, transformacaoId, (770, 770))
+    imgIdArrumada = imgIdArrumada[80:740, 10:760]
+
+    # Exibir retângulos detectados (TEST)
+    if pontasRetanguloGabarito.size != 0 and pontasRetanguloId.size != 0:
+        cv2.drawContours(imgInteresses, pontasRetanguloGabarito, -1, (255, 0, 0), 5)
+        cv2.drawContours(imgInteresses, pontasRetanguloId, -1, (0, 0, 255), 5)
+
+    # Limiar marcados
+    imgIdCinza = cv2.cvtColor(imgIdArrumada, cv2.COLOR_BGR2GRAY)
+    imgIdLimiar = cv2.threshold(imgIdCinza, 170, 255, cv2.THRESH_BINARY_INV)[1]
+    imgGabaritoCinza = cv2.cvtColor(imgGabaritoArrumada, cv2.COLOR_BGR2GRAY)
+    imgGabaritoLimiar = cv2.threshold(
+        imgGabaritoCinza, 170, 255, cv2.THRESH_BINARY_INV
+    )[1]
+
+    # # Exibir (TEST)
+    # cv2.imshow("Contornos detectados", imgContorno)
+    # cv2.imshow("Interesses", imgInteresses)
+    # cv2.imshow("Gabarito Aguia", imgGabaritoArrumada)
+    # cv2.imshow("Id Aguia", imgIdArrumada)
+    # cv2.imshow("Gabarito Cinza Limiar", imgGabaritoLimiar)
+    # # cv2.imshow("marc", marcs[0][0])
+    # while True:
+    #     if cv2.waitKey(25) & 0xFF == ord("q"):
+    #         break
+    # cv2.destroyAllWindows()
+
+    return imgIdLimiar, imgGabaritoLimiar
+
+
+def analisaProva(caminho):
+    # Processando imagem
+    img = cv2.imread(caminho)
+    # try:
+    imgIdLimiar, imgGabaritoLimiar = processaProvas(img)
+    # except Exception as e:
+    #     utlis.erro(4, caminho)
+    #     print(f"Erro no processamento da Prova! Erro: {e}")
+    #     return 4
+
+    # cv2.imshow("id", imgIdLimiar)
+    # cv2.imshow("gab", imgGabaritoLimiar)
+    # while True:
+    #     if cv2.waitKey(25) & 0xFF == ord("q"):
+    #         break
+
+    # Ler Gabarito
+    try:
+        marcsGab = utlis.separarGabarito(imgGabaritoLimiar)
+    except Exception as e:
+        utlis.erro(1, caminho)
+        print(f"Erro na leitura do Gabarito! Erro: {e}")
+        return 1
+
+    respostas = np.zeros(utlis.QUESTOES)
+    k = 0
+    while k < utlis.COLUNAS_GABARITO:  # Itera colunas
+        j = 0
+        while j < utlis.QUESTOES_POR_COLUNA:  # Itera linhas na coluna
+            i = 0
+            resposta = 0
+            while i < utlis.ALTERNATIVAS:  # Itera alternativas da questão
+                pixeis = cv2.countNonZero(marcsGab[j][i + (k * utlis.ALTERNATIVAS)])
+
+                # DEBUG
+                # cv2.imshow("atual", marcs[j][i + (k * utlis.ALTERNATIVAS)])
+                # print(pixeis)
+                # print("i = ", i, " j = ", j, " k = ", k)
+                # while True:
+                #     if cv2.waitKey(25) & 0xFF == ord("q"):
+                #         break
+
+                if pixeis >= LIMITE_GABARITO:
+                    if resposta != 0:  # Marcou o gabarito errado = anula
+                        # print("Resposta inválida!")
+                        resposta = 0
+                        break
+                    resposta = i + 1
+                i += 1
+            respostas[k * utlis.QUESTOES_POR_COLUNA + j] = resposta
+            # print("Anotando resposta", resposta, " em ", k * utlis.QUESTOES_POR_COLUNA + j)
+            j += 1
+        k += 1
+
+    try:
+        marcsId = utlis.separarId(imgIdLimiar)
+    except Exception as e:
+        utlis.erro(2, caminho)
+        print(f"Erro na leitura do Id! Erro: {e}")
+        return 2
+
+    idAluno = np.ones(utlis.COLUNAS_ID) * -1
+    k = 0
+    while k < 10:  # Linhas da identificação
         i = 0
-        resposta = 0
-        while i < utlis.ALTERNATIVAS:  # Itera alternativas da questão
-            cv2.imshow("atual", marcs[j][i + (k * utlis.ALTERNATIVAS)])
-            pixeis = cv2.countNonZero(marcs[j][i + (k * utlis.ALTERNATIVAS)])
-            print(pixeis)
-            print("i = ", i, " j = ", j, " k = ", k)
-            while True:
-                if cv2.waitKey(25) & 0xFF == ord("q"):
-                    break
-            if pixeis >= utlis.LIMITE:
-                if resposta != 0:  # Marcou o gabarito errado = anula
-                    print("Resposta inválida!")
-                    resposta = 0
-                    break
-                resposta = i + 1
+        while i < utlis.COLUNAS_ID:  # Marcações na linha
+            pixeis = cv2.countNonZero(marcsId[k][i])
+
+            # DEBUG
+            # cv2.imshow("atual", marcsId[k][i])
+            # print(pixeis)
+            # print("i = ", i, " k = ", k)
+            # while True:
+            #     if cv2.waitKey(25) & 0xFF == ord("q"):
+            #         break
+
+            if pixeis >= LIMITE_ID:
+                if idAluno[i] != -1:
+                    utlis.erro(2, caminho)
+                    print(f"Erro na leitura do Id ou Id Inválido!")
+                    return 2
+                if k == 9:
+                    # print(f"Escrevendo 0 na pos {i} do id")
+                    idAluno[i] = 0
+                else:
+                    # print(f"Escrevendo {k + 1} na pos {i} do id")
+                    idAluno[i] = k + 1
             i += 1
-        respostas[k * utlis.QUESTOES_POR_COLUNA + j] = resposta
-        print("Anotando resposta", resposta, " em ", k * utlis.QUESTOES_POR_COLUNA + j)
-        j += 1
-    k += 1
+        k += 1
+    if -1 in idAluno:
+        utlis.erro(2, caminho)
+        print(f"Erro na leitura do Id ou Id Inválido após leitura completa!")
+        return 2
 
-idAluno = 0
-k = 0
+    utlis.escreveSaida(idAluno, respostas)
 
-# Exibir (TEST)
-# cv2.imshow("Contornos detectados", imgContorno)
-# cv2.imshow("Interesses", imgInteresses)
-# cv2.imshow("Gabarito Aguia", imgGabaritoArrumada)
-# cv2.imshow("Id Aguia", imgIdArrumada)
-# cv2.imshow("Gabarito Cinza Limiar", imgGabaritoLimiar)
-# cv2.imshow("marc", marcs[0][0])
+    return 0
 
-while True:
-    if cv2.waitKey(25) & 0xFF == ord("q"):
-        break
 
-cv2.destroyAllWindows()
+main()
--- utlis.py
+++ utlis.py
@@ -1,3 +1,7 @@
+import csv
+import os
+import shutil
+
 import cv2
 import numpy as np
 
@@ -6,7 +10,6 @@ COLUNAS_GABARITO = 2
 QUESTOES_POR_COLUNA = int(QUESTOES / COLUNAS_GABARITO)
 ALTERNATIVAS = 4
 COLUNAS_ID = 5
-LIMITE = 1900
 
 
 def retContorno(contornos):
@@ -52,9 +55,74 @@ def separarGabarito(gabarito):
             np.hsplit(i, COLUNAS_GABARITO + COLUNAS_GABARITO * ALTERNATIVAS)
         )
         # marcacoes = np.hsplit(i, COLUNAS_GABARITO)
+
+    # Isso vai precisar de melhorias! Dropa as colunas "inúteis" (espaços em
+    # branco números de questões)
+    i = 0
+    while i <= (COLUNAS_GABARITO - 1) * ALTERNATIVAS:
+        marcacoes = np.delete(marcacoes, i, 1)  # Deleta a coluna inútil
+        i += ALTERNATIVAS
     return marcacoes
 
 
-def separarId():
+def separarId(id):
+    linhas = np.vsplit(id, 10)  # Caixa dos alunos + marcações
+    # linhas = np.delete(linhas, 0, 0)  # Deleta linha do id escrito
+    marcacoes = []
+    for i in linhas:
+        marcacoes.append(np.hsplit(i, COLUNAS_ID))
 
-    return
+    return marcacoes
+
+
+def erro(tipo, arquivo):
+    """
+    Função para jogar erros. Altera o nome do arquivo da prova e coloca em uma
+    pasta do erro específico.
+        1. Erro na leitura da prova (não encontrou gabarito)
+        2. Erro na leitura da prova (não encontrou id)
+        3. Erro na leitura de id ou id inválido
+        4. Erro no processamento da prova
+    """
+    nomeArqv = os.path.basename(arquivo)
+    match tipo:
+        case 1:
+            # shutil.move(arquivo, f"./ErroGabarito/{nomeArqv}")
+            return 1
+        case 2:
+            # shutil.move(arquivo, f"./ErroId/{nomeArqv}")
+            return 2
+        case 3:
+            # shutil.move(arquivo, f"./ErroId/{nomeArqv}")
+            return 3
+        case 4:
+            # shutil.move(arquivo, f"./ErroProc/{nomeArqv}")
+            return 4
+
+
+# Deve ter um jeito melhor de fazer isso
+def escreveSaida(id, resposta):
+    """
+    Escreve em um arquivo .csv a saída da leitura da prova
+    """
+    id = map(int, id)
+    id = "".join(map(str, id))
+    dados = {"id": id}
+    resposta = map(int, resposta)
+    respostas = dict(enumerate(resposta, 1))
+    dados.update(respostas)
+    print(dados)
+    with open("./SaidaProvas/resultados.csv", "a") as saidacsv:
+        writer = csv.writer(saidacsv)
+        writer.writerow(dados.values())
+        print(f"Escrevendo dados {dados}")
+
+
+def preparaCSV():
+    """
+    Prepara o CSV para receber dados
+    """
+    with open("./SaidaProvas/resultados.csv", "w") as saidacsv:
+        campos = ["id", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10"]
+        writer = csv.DictWriter(saidacsv, fieldnames=campos)
+        writer.writeheader()
